129bfd371e8b073f322294bc6e1947b1d7406313
Nasim Koohestani <nasimkoohestani82@gamil.com>
2025-07-19 18:16:04 +0330

    Add and update all Array & Hashing problem solutions

--- Array & Hashing/two_sum.py
+++ Array & Hashing/two_sum.py
@@ -3,6 +3,7 @@ class Solution:
         seen={}
         # seen is a dictionary that for each value -> index
         for i,n in enumerate(nums):
+            #enumerate means index and value
             different=target-n
             if different in seen:
                 return [seen[different],i]
